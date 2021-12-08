380f364110b850042d1d8872b624b1131509609d
sarabjeets <sarabjeetsinghdigwa@gmail.com>
2021-12-08 16:29:36 +0530

    Refactor:Update nodemailer to generate random string and expiry Time

--- App/utilities/nodemailer.js
+++ App/utilities/nodemailer.js
@@ -1,37 +1,44 @@
+/**
+ * @description used to send email to the user
+ * @param {*} email
+ * @param {*} subject
+ * @returns
+ */
 const nodemailer = require('nodemailer');
 require('dotenv').config();
-const Otp=require('../models/otp.js');
+const Otp = require('../models/otp.model');
 
-exports.sendEmail = (mailMessage) => {
-  let otpcode=Math.random().toString(36).substring(2,12);
-  let optData=new Otp({
-    email:mailMessage.email,
-    code:otpcode,
-    expireIn:new Date().getTime()+300*1000
-  })
-  optData.save();
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD
-    }
-  });
+exports.sendEmail = (data) => {
+    let otpcode = Math.random().toString(36).substring(2, 12);
+    let optData = new Otp({
+        email: data.email,
+        code: otpcode,
+        expireIn: new Date().getTime() + 300 * 1000
+    })
+    optData.save();
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.pass
+        }
+    });
 
-  const message = {
-    from: process.env.EMAIL,
-    to: mailMessage.email,
-    subject:'Fundoo notes otp code',
-    html:`Enter this otp to reset your password
-    <h3>${otpcode}</h3>`
-  };
+    const message = {
+        from: process.env.EMAIL,
+        to: data.email,
+        subject:'Fundoo notes otp code',
+        html:`Enter this otp to reset your password
+        <h3>${otpcode}</h3>`
 
-  transporter.sendMail(message, (err, info) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('email has been sent', info.response);
-      return info.response;
-    }
-  });
-}
+    };
+
+    transporter.sendMail(message, (err, info) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('email has been sent', info.response);
+            return info.response;
+        }
+    });
+};
