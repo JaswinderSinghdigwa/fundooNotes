64f714d5f05eb0de07a5f4848903ba09125dfbaa
sarabjeets <sarabjeetsinghdigwa@gmail.com>
2022-01-10 09:13:20 +0530

    Refactor : Implementation of code is changed

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors =require('cors');
 dotenv.config();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/swagger.json');
 
 // create express app
 const app = express();
+app.use(cors())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
